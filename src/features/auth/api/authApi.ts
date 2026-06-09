0101ab4e53c0428f45d6e079ba1b375ecfbfe745
Clacki <skthk6613@naver.com>
2026-06-09 16:21:43 +0900

    feat: 로그아웃 api 연결

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,6 +1,7 @@
 import api from '@/lib/api'
 
 const TOKEN_REFRESH_PATH = '/auth/token/refresh'
+const LOGOUT_PATH = '/auth/logout'
 
 export type TokenRefreshResponse = {
   access_token: string
@@ -10,4 +11,8 @@ export const refreshAccessToken = async () => {
   const response = await api.post<TokenRefreshResponse>(TOKEN_REFRESH_PATH)
 
   return response.data
+}
+
+export const logout = async () => {
+  await api.post(LOGOUT_PATH)
 }
